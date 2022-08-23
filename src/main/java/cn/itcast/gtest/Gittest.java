3acbeedc7230047668a31394b4c5a63d58e13b71
xinyangrui <101245457+xinyangrui@users.noreply.github.com>
2022-08-23 15:32:30 +0800

    pull commit

--- src/main/java/cn/itcast/gtest/Gittest.java
+++ src/main/java/cn/itcast/gtest/Gittest.java
@@ -4,5 +4,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("push test");
+        System.out.println("pull test;请在push之前先拉取pull");
     }
 }
