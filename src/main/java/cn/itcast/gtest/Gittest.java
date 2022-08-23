9a80a0812d6b7aec9f7ccd3034f3e1cc7d654e75
Fengrui <xinyangfr@126.com>
2022-08-23 14:42:39 +0800

    Push commit

--- src/main/java/cn/itcast/gtest/Gittest.java
+++ src/main/java/cn/itcast/gtest/Gittest.java
@@ -3,5 +3,6 @@ package cn.itcast.gtest;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("push test");
     }
 }
